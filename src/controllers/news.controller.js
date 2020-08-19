c9735be09b070a74cb93bbad60298958146c7580
vadvit009 <vadvit009@gmail.com>
2020-08-19 18:42:46 +0300

    new pagination for news

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -27,8 +27,8 @@ const upload = multer({ storage: storage });
 const getAllNews = async (req, res) => {
   const { skip } = req.query;
   return await News.find()
-    .skip(skip ? (skip - 1) * 3 : 0)
-    .limit(3)
+    .skip(skip ? (skip - 1) * 24 : 0)
+    .limit(24)
     .sort({ createdAt: -1 })
     .exec((err, news) => {
       if (err) return res.send(err);
