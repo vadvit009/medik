776491ba5e6e85508d1623daaf27c4c68c56d509
vadvit009 <vadvit009@gmail.com>
2020-08-25 20:44:48 +0300

    update regex

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -65,9 +65,9 @@ const getAllProducts = async (req, res) => {
         $match: {
           // $or: [
           //   {
-              title: {
-                $regex: `/${search}$/`
-              }
+          title: {
+            $regex: search
+          }
           //   },
           //   {
           //     $text: {
