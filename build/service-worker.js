7725d7e6318063a9168d137d85612c7c48445dd4
vadvit009 <vadvit009@gmail.com>
2020-09-18 01:11:25 +0300

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af281a1f827123be55f62fbfbb755927.js"
+  "/precache-manifest.3adccf9919cedd3fb5d742a7fc33fee0.js"
 );
 
 self.addEventListener('message', (event) => {
