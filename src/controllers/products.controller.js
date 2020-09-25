8a031036e86c558c6d4521c9a2333419eb3c3e90
vadvit009 <vadvit009@gmail.com>
2020-09-25 18:13:20 +0300

    fix update product

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,316 +1,338 @@
-const {Product} = require("../models");
-const {ObjectId} = require("mongoose").Types;
+const { Product } = require("../models");
+const { ObjectId } = require("mongoose").Types;
 
 const getAllProducts = async (req, res) => {
-    const {search, page, category, sort} = req.query;
-    const sortBy = () => {
-        if (sort === undefined) return {title: -1};
-        if (sort === "recommended") return {recommended: -1};
-        if (sort === "priceMinus") return {price: 1};
-        if (sort === "price") return {price: -1};
-    };
-    console.log("SEARCH === " + search + " PAGE === " + page);
-    console.log("CATEGORY === " + category);
+  const { search, page, category, sort } = req.query;
+  const sortBy = () => {
+    if (sort === undefined) return { title: -1 };
+    if (sort === "recommended") return { recommended: -1 };
+    if (sort === "priceMinus") return { price: 1 };
+    if (sort === "price") return { price: -1 };
+  };
+  console.log("SEARCH === " + search + " PAGE === " + page);
+  console.log("CATEGORY === " + category);
 
-    const skipNumber = () => {
-        if (page) {
-            return 24 * (page - 1);
+  const skipNumber = () => {
+    if (page) {
+      return 24 * (page - 1);
+    }
+    return 0;
+  };
+
+  console.warn("SKIPNUMBER === ", skipNumber());
+
+  const categoryArrayObjectIds =
+    (category && category.split(",").map(id => ObjectId(id))) || [];
+
+  const length = await Product.countDocuments().then(length => length);
+
+  if (category) {
+    Product.aggregate([
+      {
+        $lookup: {
+          from: "category",
+          localField: "categoryID",
+          foreignField: "_id",
+          as: "categories"
         }
-        return 0;
-    }
-
-    console.warn("SKIPNUMBER === ", skipNumber());
-
-    const categoryArrayObjectIds =
-        (category && category.split(",").map((id) => ObjectId(id))) || [];
-
-    const length = await Product.countDocuments().then((length) => length);
-
-    if (category) {
-        Product.aggregate([
+      },
+      {
+        $match: {
+          $and: [
             {
-                $lookup: {
-                    from: "category",
-                    localField: "categoryID",
-                    foreignField: "_id",
-                    as: "categories",
+              $or: [
+                {
+                  "categories.parentID": { $in: categoryArrayObjectIds }
                 },
-            },
-            {
-                $match: {
-                    $and: [
-                        {
-                            $or: [
-                                {
-                                    "categories.parentID": {$in: categoryArrayObjectIds},
-                                },
-                                {
-                                    "categories.subParentID": {$in: categoryArrayObjectIds},
-                                },
-                                {
-                                    "categories._id": {$in: categoryArrayObjectIds},
-                                },
-                            ],
-                        },
-                    ],
+                {
+                  "categories.subParentID": { $in: categoryArrayObjectIds }
                 },
-            },
-        ])
-            .sort(sortBy())
-            .skip(skipNumber())
-            .limit(24)
-            .exec((err, products) => {
-                if (err) {
-                    res.sendStatus(400);
-                    return console.log(err);
+                {
+                  "categories._id": { $in: categoryArrayObjectIds }
                 }
-                res.send({products, length});
-            })
-    }
-
-    if (search) {
-        Product.aggregate([
-            {
-                $match: {
-                    title: {
-                        $regex: search
-                    }
-                }
-            },
-            {
-                $lookup: {
-                    from: "category",
-                    localField: "categoryID",
-                    foreignField: "_id",
-                    as: "categories",
-                },
-            },
-        ])
-            .sort(sortBy())
-            .skip(skipNumber())
-            .limit(24)
-            .then((products) => {
-                const productsLength = products.length;
-                res.send({products, length: productsLength});
-            })
-            .catch((err) => {
-                console.log(err)
-                res.sendStatus(400);
-            });
-    } else {
-        Product.aggregate([
-            {
-                $lookup: {
-                    from: "category",
-                    localField: "categoryID",
-                    foreignField: "_id",
-                    as: "categories",
-                },
+              ]
             }
-        ])
-            .sort(sortBy())
-            .skip(skipNumber())
-            .limit(24)
-            .then((products) => {
-                const productsLength = products.length;
-                res.send({products, length: productsLength});
-            })
-            .catch((err) => {
-                console.log(err)
-                res.sendStatus(400);
-            });
-    }
+          ]
+        }
+      }
+    ])
+      .sort(sortBy())
+      .skip(skipNumber())
+      .limit(24)
+      .exec((err, products) => {
+        if (err) {
+          res.sendStatus(400);
+          return console.log(err);
+        }
+        res.send({ products, length });
+      });
+  }
+
+  if (search) {
+    const searchedLenght = Product.aggregate([
+      {
+        $match: {
+          title: {
+            $regex: search
+          }
+        }
+      },
+      {
+        $lookup: {
+          from: "category",
+          localField: "categoryID",
+          foreignField: "_id",
+          as: "categories"
+        }
+      }
+    ]).then(prods => console.log(prods.lenght));
+
+    Product.aggregate([
+      {
+        $match: {
+          title: {
+            $regex: search
+          }
+        }
+      },
+      {
+        $lookup: {
+          from: "category",
+          localField: "categoryID",
+          foreignField: "_id",
+          as: "categories"
+        }
+      }
+    ])
+      .sort(sortBy())
+      .skip(skipNumber())
+      .limit(24)
+      .then(products => {
+        res.send({ products, length });
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  } else {
+    Product.aggregate([
+      {
+        $lookup: {
+          from: "category",
+          localField: "categoryID",
+          foreignField: "_id",
+          as: "categories"
+        }
+      }
+    ])
+      .sort(sortBy())
+      .skip(skipNumber())
+      .limit(24)
+      .then(products => {
+        const productsLength = products.length;
+        res.send({ products, length: productsLength });
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  }
 };
 
 const getProduct = async (req, res) => {
-    const {id} = req.params;
-    return await Product.findById(id)
-        .populate("vendorID", "title -_id")
-        .populate({
-            path: "categoryID",
-            select: "subParentID title desc",
-            populate: {
-                path: "subParentID",
-                select: "title",
-                populate: {
-                    path: "parentID",
-                    select: "title",
-                },
-            },
-        })
-        .populate({
-            path: "reviews",
-            select: "title desc rating userID -_id",
-            populate: {path: "userID", select: "fName -_id"},
-        })
-        .exec((err, prod) => {
-            if (err) return res.send(err);
-            res.send(prod);
-        });
-};
-
-const createProduct = async (req, res) => {
-    const {
-        title,
-        desc,
-        price,
-        salePrice,
-        attr,
-        attrOptions,
-        categoryID,
-        vendorID,
-        recommended,
-        visibility,
-        quantity,
-        reviews,
-        article,
-        gallery,
-    } = req.body;
-    return await Product.create({
-        title,
-        desc,
-        price,
-        salePrice,
-        attr,
-        attrOptions,
-        reviews,
-        categoryID,
-        vendorID,
-        recommended,
-        visibility,
-        quantity,
-        article,
-        gallery,
-        createdAt: Date.now(),
-        updatedAt: Date.now(),
-        deletedAt: null,
+  const { id } = req.params;
+  return await Product.findById(id)
+    .populate("vendorID", "title -_id")
+    .populate({
+      path: "categoryID",
+      select: "subParentID title desc",
+      populate: {
+        path: "subParentID",
+        select: "title",
+        populate: {
+          path: "parentID",
+          select: "title"
+        }
+      }
     })
-        .then((product) => {
-            res.json(product);
-        })
-        .catch((err) => res.send({error: true, errMsg: err}));
-};
-
-const updateProduct = (req, res) => {
-    const {id} = req.params;
-    const {
-        title,
-        desc,
-        price,
-        salePrice,
-        attr,
-        categoryID,
-        vendorID,
-        recommended,
-        visibility,
-        quantity,
-        article,
-        gallery,
-    } = req.body;
-    return Product.findByIdAndUpdate(id, {
-        title,
-        desc,
-        price,
-        salePrice,
-        attr,
-        categoryID,
-        vendorID,
-        recommended,
-        visibility,
-        quantity,
-        article,
-        gallery,
-        updatedAt: Date.now(),
-        deletedAt: null,
+    .populate({
+      path: "reviews",
+      select: "title desc rating userID -_id",
+      populate: { path: "userID", select: "fName -_id" }
     })
-        .then((product) => {
-            res.json(product);
-        })
-        .catch((err) => res.send({error: true, errMsg: err}));
-};
-
-const restoreProduct = async (req, res) => {
-    const {id} = req.params;
-    return await Product.findByIdAndUpdate(id,
-        {deletedAt: null}
-    )
-        .then((product) => {
-            res.json(product);
-        })
-        .catch((err) => {
-            res.sendStatus(400);
-            console.log("err === ", err)
-        });
-};
-
-const softDeleteProduct = async (req, res) => {
-    const {id} = req.params;
-    return await Product.findByIdAndUpdate(id,
-        {deletedAt: Date.now()}
-    )
-        .then((product) => {
-            res.json(product);
-        })
-        .catch((err) => console.log("err === ", err));
-};
-
-const deleteProduct = async (req, res) => {
-    const {id} = req.params;
-    return await Product.findByIdAndDelete(id).then((product) => {
-        res.json(product);
+    .exec((err, prod) => {
+      if (err) return res.send(err);
+      res.send(prod);
     });
 };
 
+const createProduct = async (req, res) => {
+  const {
+    title,
+    desc,
+    price,
+    salePrice,
+    attr,
+    attrOptions,
+    categoryID,
+    vendorID,
+    recommended,
+    visibility,
+    quantity,
+    reviews,
+    article,
+    gallery
+  } = req.body;
+  return await Product.create({
+    title,
+    desc,
+    price,
+    salePrice,
+    attr,
+    attrOptions,
+    reviews,
+    categoryID,
+    vendorID,
+    recommended,
+    visibility,
+    quantity,
+    article,
+    gallery,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+    deletedAt: null
+  })
+    .then(product => {
+      res.json(product);
+    })
+    .catch(err => res.send({ error: true, errMsg: err }));
+};
+
+const updateProduct = (req, res) => {
+  const { id } = req.params;
+  const {
+    title,
+    desc,
+    price,
+    salePrice,
+    attr,
+    attrOptions,
+    categoryID,
+    vendorID,
+    recommended,
+    visibility,
+    quantity,
+    article,
+    gallery
+  } = req.body;
+  return Product.findByIdAndUpdate(id, {
+    title,
+    desc,
+    price,
+    salePrice,
+    attr,
+    attrOptions,
+    categoryID,
+    vendorID,
+    recommended,
+    visibility,
+    quantity,
+    article,
+    gallery,
+    updatedAt: Date.now(),
+    deletedAt: null
+  })
+    .then(product => {
+      res.json(product);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+};
+
+const restoreProduct = async (req, res) => {
+  const { id } = req.params;
+  return await Product.findByIdAndUpdate(id, { deletedAt: null })
+    .then(product => {
+      res.json(product);
+    })
+    .catch(err => {
+      res.sendStatus(400);
+      console.log("err === ", err);
+    });
+};
+
+const softDeleteProduct = async (req, res) => {
+  const { id } = req.params;
+  return await Product.findByIdAndUpdate(id, { deletedAt: Date.now() })
+    .then(product => {
+      res.json(product);
+    })
+    .catch(err => console.log("err === ", err));
+};
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  return await Product.findByIdAndDelete(id).then(product => {
+    res.json(product);
+  });
+};
+
 const getProductHighScore = (req, res) => {
-    return Product.aggregate([
-        {
-            $lookup: {
-                from: "reviews",
-                localField: "reviews",
-                foreignField: "_id",
-                as: "reviews"
-            },
-        },
-        {
-            $match: {
-                "reviews.rating": {$gte: 4}
-            }
-        }
-    ])
-        .then(products => res.send(products))
-        .catch(err => {
-                console.log(err);
-                res.sendStatus(400)
-            }
-        )
+  return Product.aggregate([
+    {
+      $lookup: {
+        from: "reviews",
+        localField: "reviews",
+        foreignField: "_id",
+        as: "reviews"
+      }
+    },
+    {
+      $match: {
+        "reviews.rating": { $gte: 4 }
+      }
+    }
+  ])
+    .then(products => res.send(products))
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+    });
 };
 
 const getProductByArrayOfIds = (req, res) => {
-    const {productsArray} = req.query;
-    const productsArrayObjectIds = (productsArray.length && productsArray.split(",").map(product => product.length && ObjectId(product))) || [];
+  const { productsArray } = req.query;
+  const productsArrayObjectIds =
+    (productsArray.length &&
+      productsArray
+        .split(",")
+        .map(product => product.length && ObjectId(product))) ||
+    [];
 
-    return Product.find({
-        _id: {
-            $in: productsArrayObjectIds
-        }
+  return Product.find({
+    _id: {
+      $in: productsArrayObjectIds
+    }
+  })
+    .then(products => {
+      res.send(products);
     })
-        .then(products => {
-            res.send(products)
-        })
-        .catch(err => {
-            console.log("ERROR WHEN GET_PRODUCTS_BY_ID === ", err);
-            res.sendStatus(400)
-        })
-}
+    .catch(err => {
+      console.log("ERROR WHEN GET_PRODUCTS_BY_ID === ", err);
+      res.sendStatus(400);
+    });
+};
 
 module.exports = {
-    getProductHighScore,
-    getAllProducts,
-    getProduct,
-    createProduct,
-    deleteProduct,
-    softDeleteProduct,
-    restoreProduct,
-    updateProduct,
-    getProductByArrayOfIds
+  getProductHighScore,
+  getAllProducts,
+  getProduct,
+  createProduct,
+  deleteProduct,
+  softDeleteProduct,
+  restoreProduct,
+  updateProduct,
+  getProductByArrayOfIds
 };
