8fdb3901e992c5fe7ca8d4949aa4fae340fd1ec8
vadvit009 <vadvit009@gmail.com>
2020-10-31 14:05:44 +0200

    gmail send email works

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,85 @@ const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const {restorePassword} = require("../mailer");
 const {ObjectId} = require("mongoose").Types;
+const {google} = require('googleapis');
+const fs = require('fs');
+const readline = require('readline');
+
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send'];
+const TOKEN_PATH = 'token.json';
+fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    authorize(JSON.parse(content), listLabels);
+});
+
+function authorize(credentials, callback) {
+    console.log(credentials.web)
+    const {client_secret, client_id, redirect_uris} = credentials.web;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getNewToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
+}
+
+function getNewToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error retrieving access token', err);
+            oAuth2Client.setCredentials(token);
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err);
+                console.log('Token stored to', TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
+}
+
+function listLabels(auth) {
+    const gmail = google.gmail({version: 'v1', auth});
+    gmail.users.labels.list({
+        userId: 'me',
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const labels = res.data.labels;
+        if (labels.length) {
+            console.log('Labels:');
+            labels.forEach((label) => {
+                console.log(`- ${label.name}`);
+            });
+        } else {
+            console.log('No labels found.');
+        }
+    });
+}
+
+function makeBody(to, from, subject, message) {
+    var str = ["Content-Type: text/html; charset=\"UTF-8\"\n",
+        "MIME-Version: 1.0\n",
+        "Content-Transfer-Encoding: 7bit\n",
+        "to: ", to, "\n",
+        "from: ", from, "\n",
+        "subject: ", subject, "\n\n",
+        message
+    ].join('');
+
+    var encodedMail = new Buffer(str).toString("base64").replace(/\+/g, '-').replace(/\//g, '_');
+    return encodedMail;
+}
 
 module.exports = {
     getAllUsers: async (req, res) => {
@@ -144,7 +223,24 @@ module.exports = {
         return User.findOneAndUpdate({email: email}, {password: randomizerPass})
             .then((user) => {
                 if (user) {
-                    restorePassword(email, randomizerPass);
+                    function sendMessage(auth) {
+                        const gmail = google.gmail({version: 'v1', auth});
+                        const raw = makeBody(email, 'medtechnika.te@gmail.com', 'Відновлення паролю', `<a href="https://medtechnika.te.ua/restore/${randomizerPass}">Натисніть для відновлення паролю</a>>`);
+                        gmail.users.messages.send({
+                            auth: auth,
+                            userId: 'me',
+                            resource: {
+                                raw: raw
+                            }
+                        }, function (err, response) {
+                            console.log(err || response)
+                        });
+                    }
+
+                    fs.readFile('credentials.json', (err, content) => {
+                        if (err) return console.log('Error loading client secret file:', err);
+                        authorize(JSON.parse(content), sendMessage);
+                    });
                     // jwt.sign()
                     res.sendStatus(200);
                 } else {
